907726b4d1f706dd07d3bc42a87f9e29270c2c08
YahyaOmari <yahya.omari@outlook.com>
2020-12-16 13:13:30 +0200

    last update

--- gym.js
+++ gym.js
@@ -3,7 +3,7 @@ alert("Now I want to take some information from you.");
 var name = prompt("Please enter your name.");
 var number = prompt("Enter your weight " + name + ".");
 
-alert("How much do you want to lose weight " + name + "?");
+alert("How much do you want to lose weight " + name + "? Press Ok to continue please.");
 var number2 = prompt("Enter how much?");
 
 
